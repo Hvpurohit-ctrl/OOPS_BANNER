5b276a6217aca491c63c0dad0fecac8d98d1c780
Harshvardhan Purohit <purohitharshvardhan74@gmail.com>
2026-03-07 21:49:08 +0530

    commit 2

---
+++ UC2.java
@@ -0,0 +1,17 @@
+/* 
+@version 2.0
+@autor Harshvardhan
+ */
+public class UC2 {
+    public static void main(String[] args) {
+
+        System.out.println("    ***    "+"    ***    "+" ******  "+ "   ***** ");
+        System.out.println("  **   **  "+"  **   **  "+" **   ** "+ " **      ");
+        System.out.println(" **     ** "+" **     ** "+" **   ** "+ "***      ");
+        System.out.println(" **     ** "+" **     ** "+" ******  "+ "  *****  ");
+        System.out.println(" **     ** "+" **     ** "+" **      "+ "       **");
+        System.out.println("  **   **  "+"  **   **  "+" **      "+ "**     **");
+        System.out.println("    ***    "+"    ***    "+" **      "+ "  *****  ");;
+
+    }
+}
