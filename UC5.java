9d69091eff6f86aaa697cb206cadab6f1c8a8284
unknown <purohitharshvardhan74@gmail.com>
2026-03-10 01:41:47 +0530

    added UC5

---
+++ UC5.java
@@ -0,0 +1,23 @@
+/* 
+@version 5.0
+@autor Harshvardhan
+ */
+public class UC5 {
+
+    public static void main(String[] args) {
+
+        String[] lines = {
+            "    ***        ***      ******      *****  ",  
+            "  **   **    **   **    **   **   **       ",   
+            " **     **  **     **   **   **  ***       ",  
+            " **     **  **     **   ******     *****   ", 
+            " **     **  **     **   **              ** ",  
+            "  **   **    **   **    **       **     ** ",   
+            "    ***        ***      **         *****   "   
+        };
+
+        for (String line : lines) {
+            System.out.println(line);
+        }
+    }
+}
