f534484fd152c7b9a4a719085854cd1a559e1f1b
unknown <purohitharshvardhan74@gmail.com>
2026-03-10 00:41:13 +0530

    Added UC3

---
+++ UC3.java
@@ -0,0 +1,17 @@
+/* 
+@version 2.0
+@autor Harshvardhan
+ */
+public class UC3 {
+    public static void main(String[] args) {
+
+        System.out.println(String.join(" ","    ***    ","    ***    "," ****** ", "   ***** "));
+        System.out.println(String.join(" ","  **   **  ","  **   **  "," **   **", " **      "));
+        System.out.println(String.join(" "," **     ** "," **     ** "," **   **", "***      "));
+        System.out.println(String.join(" "," **     ** "," **     ** "," ****** ", "  *****  "));
+        System.out.println(String.join(" "," **     ** "," **     ** "," **     ", "       **"));
+        System.out.println(String.join(" ","  **   **  ","  **   **  "," **     ", "**     **"));
+        System.out.println(String.join(" ","    ***    ","    ***    "," **     ", "  *****  "));
+
+    }
+}
